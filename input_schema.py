00d8d95dbc6fc64f5c7788b70b75a1c0d807b69e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-12 16:38:45 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,23 +4,5 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["https://cdn-media.huggingface.co/speech_samples/sample2.flac"]
-    },
-    "language": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["hi"]
-    },
-    "task": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["transcribe"]
-    },
-    "return_timestamps": {
-        'datatype': 'BOOL',
-        'required': True,
-        'shape': [1],
-        'example': [true]
     }
 }
