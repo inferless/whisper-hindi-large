baa8bafac75edc7c1a97de1ef72a131359fec0d9
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-05 15:43:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+from huggingface_hub import snapshot_download
 from transformers import pipeline
 
 class InferlessPythonModel:
     def initialize(self):
+        snapshot_download(repo_id="vasista22/whisper-hindi-large-v2",allow_patterns=["*.safetensors"])
         self.generator = pipeline("automatic-speech-recognition", model="vasista22/whisper-hindi-large-v2",device=0)
 
     def infer(self, inputs):
